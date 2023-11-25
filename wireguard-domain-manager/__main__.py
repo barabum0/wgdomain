816eb23326f4221b4b03829b59b249ea3b3377f7
roman mac <barabum@duck.com>
2023-11-25 13:43:56 +0300

    :sparkles: feat: add config file parsing and type annotations to CLI functions

--- wireguard-domain-manager/__main__.py
+++ wireguard-domain-manager/__main__.py
@@ -1,7 +1,16 @@
+import configparser
+
 import click
 import click_completion
 
 click_completion.init()
+
+
+def parse_config_file(config_file_path: str) -> configparser.ConfigParser:
+    """Открывает и парсит файл конфигурации Wireguard."""
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+    return config
 
 
 @click.group()
@@ -13,7 +22,7 @@ def cli():
 @cli.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
 @click.argument('domain')
-def include(config_file, domain):
+def include(config_file: str, domain: str):
     """Includes a domain or IP for routing through Wireguard."""
     # Code to include domain/IP
     click.echo(f"Including {domain} in {config_file}.")
@@ -22,7 +31,7 @@ def include(config_file, domain):
 @cli.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
 @click.argument('domain')
-def exclude(config_file, domain):
+def exclude(config_file: str, domain: str):
     """Excludes a domain or IP from routing through Wireguard."""
     # Code to exclude domain/IP
     click.echo(f"Excluding {domain} from {config_file}.")
@@ -36,7 +45,7 @@ def reset():
 
 @reset.command(name='all')
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
-def reset_all(config_file):
+def reset_all(config_file: str):
     """Resets configuration to accept all domains."""
     # Code to reset configuration to accept all domains
     click.echo(f"Resetting {config_file} to accept all domains.")
@@ -44,7 +53,7 @@ def reset_all(config_file):
 
 @reset.command(name='none')
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
-def reset_none(config_file):
+def reset_none(config_file: str):
     """Resets configuration to exclude all domains."""
     # Code to reset configuration to exclude all domains
     click.echo(f"Resetting {config_file} to exclude all domains.")
