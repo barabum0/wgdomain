c27b6d8af3f6396a71683e8ca5dbb834bf3adaba
roman mac <barabum@duck.com>
2023-11-25 15:32:14 +0300

    :gear: feat: add QR code generation and no-save option to reset-none command

--- wgdomain/commands/reset/none.py
+++ wgdomain/commands/reset/none.py
@@ -1,17 +1,24 @@
 import click
 
 from wgdomain.tools.config import parse_config_file
+from wgdomain.tools.qr import print_qr
 
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
-def reset_none(config_file: str):
+@click.option('--qr', is_flag=True, default=False, help="Generate a QR code of a config file")
+@click.option('--no-save', is_flag=True, default=False, help="Don't save the config to file")
+def reset_none(config_file: str, qr: bool, no_save: bool):
     """Resets configuration to exclude all domains."""
 
     config = parse_config_file(config_file)
     config.remove_option("Peer", "AllowedIPs")
 
-    with open(config_file, "w") as cfg_file:
-        config.write(cfg_file)
+    if not no_save:
+        with open(config_file, "w") as cfg_file:
+            config.write(cfg_file)
 
     click.echo(f"Resetting {config_file} to exclude all domains.")
+
+    if qr:
+        print_qr(config)
