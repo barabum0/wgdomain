8adf0e91c926f6df3d74a70b8876f4b00c7a1ecd
roman mac <barabum@duck.com>
2023-11-25 13:39:07 +0300

    :recycle: refactor: restructure reset command into a command group

--- wireguard-domain-manager/__main__.py
+++ wireguard-domain-manager/__main__.py
@@ -28,17 +28,26 @@ def exclude(config_file, domain):
     click.echo(f"Excluding {domain} from {config_file}.")
 
 
-@cli.command()
+@cli.group()
+def reset():
+    """Resets configuration settings."""
+    pass
+
+
+@reset.command(name='all')
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
-@click.argument('setting', type=click.Choice(['all', 'none'], case_sensitive=False))
-def reset(config_file, setting):
-    """Resets configuration to accept all or no domains."""
-    if setting == 'all':
-        # Code to reset configuration to accept all domains
-        click.echo(f"Resetting {config_file} to accept all domains.")
-    elif setting == 'none':
-        # Code to reset configuration to exclude all domains
-        click.echo(f"Resetting {config_file} to exclude all domains.")
+def reset_all(config_file):
+    """Resets configuration to accept all domains."""
+    # Code to reset configuration to accept all domains
+    click.echo(f"Resetting {config_file} to accept all domains.")
+
+
+@reset.command(name='none')
+@click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
+def reset_none(config_file):
+    """Resets configuration to exclude all domains."""
+    # Code to reset configuration to exclude all domains
+    click.echo(f"Resetting {config_file} to exclude all domains.")
 
 
 if __name__ == '__main__':
