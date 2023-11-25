e78baf45d72d26f1c296d68f2ac2682033a470e6
roman mac <barabum@duck.com>
2023-11-25 14:29:46 +0300

    :recycle: refactor(reset): implement logic to accept all domains in config

--- wgdomain/commands/reset/all.py
+++ wgdomain/commands/reset/all.py
@@ -1,9 +1,17 @@
 import click
 
+from wgdomain.tools.config import parse_config_file
+
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
 def reset_all(config_file: str):
     """Resets configuration to accept all domains."""
-    # Code to reset configuration to accept all domains
+
+    config = parse_config_file(config_file)
+    config.set("Peer", "AllowedIPs", "0.0.0.0/0, ::/0")
+
+    with open(config_file, "w") as cfg_file:
+        config.write(cfg_file)
+
     click.echo(f"Resetting {config_file} to accept all domains.")
