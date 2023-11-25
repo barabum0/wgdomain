1ba489c35deb4e7dbad20e5977b68e5351ec3dc3
roman mac <barabum@duck.com>
2023-11-25 15:02:41 +0300

    :bug: fix: maintain case sensitivity in config parser by setting `optionxform` to `str`

--- wgdomain/tools/config.py
+++ wgdomain/tools/config.py
@@ -4,5 +4,6 @@ import configparser
 def parse_config_file(config_file_path: str) -> configparser.ConfigParser:
     """Открывает и парсит файл конфигурации Wireguard."""
     config = configparser.ConfigParser()
+    config.optionxform = str
     config.read(config_file_path)
     return config
