9fe27a53debe38337a5670ddfef10ae5f256887a
roman mac <barabum@duck.com>
2023-11-25 14:29:56 +0300

    :recycle: refactor: implement config reset to exclude all domains in `reset_none` command

--- wgdomain/commands/reset/none.py
+++ wgdomain/commands/reset/none.py
@@ -1,9 +1,17 @@
 import click
 
+from wgdomain.tools.config import parse_config_file
+
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
 def reset_none(config_file: str):
     """Resets configuration to exclude all domains."""
-    # Code to reset configuration to exclude all domains
+
+    config = parse_config_file(config_file)
+    config.remove_option("Peer", "AllowedIPs")
+
+    with open(config_file, "w") as cfg_file:
+        config.write(cfg_file)
+
     click.echo(f"Resetting {config_file} to exclude all domains.")
