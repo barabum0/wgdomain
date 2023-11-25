74627da12ae7b7a5310a0b0500faa466ebc87e0d
roman mac <barabum@duck.com>
2023-11-25 13:28:32 +0300

    :recycle: refactor: replace if statements with match-case in app function

--- wireguard-domain-manager/__main__.py
+++ wireguard-domain-manager/__main__.py
@@ -8,7 +8,7 @@ click_completion.init()
 @click.argument('config_file', type=click.Path())
 @click.argument('action', type=click.Choice(['include', 'exclude'], case_sensitive=False))
 @click.argument('domain')
-def wgdomain(config_file, action, domain):
+def app(config_file, action, domain):
     """
     Управляет доменами и IP-адресами в конфигурации Wireguard.
 
@@ -16,14 +16,15 @@ def wgdomain(config_file, action, domain):
     ACTION: 'include' для добавления, 'exclude' для исключения домена/IP.
     DOMAIN: Домен или IP-адрес для обработки.
     """
-    # Логика обработки команды
-    if action == 'include':
-        # Код для добавления домена/IP
-        click.echo(f"Добавление {domain} в {config_file}.")
-    elif action == 'exclude':
-        # Код для исключения домена/IP
-        click.echo(f"Исключение {domain} из {config_file}.")
+
+    match action:
+        case "include":
+            # Код для добавления домена/IP
+            click.echo(f"Добавление {domain} в {config_file}.")
+        case "exclude":
+            # Код для исключения домена/IP
+            click.echo(f"Исключение {domain} из {config_file}.")
 
 
 if __name__ == '__main__':
-    wgdomain()
+    app()
