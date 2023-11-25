486b0407008cbf356d8a0dd78d212b57a9f9d3fe
roman mac <barabum@duck.com>
2023-11-25 15:33:51 +0300

    :gear: feat: add QR code generation and no-save options to reset command

--- wgdomain/commands/reset/all.py
+++ wgdomain/commands/reset/all.py
@@ -1,17 +1,24 @@
 import click
 
 from wgdomain.tools.config import parse_config_file
+from wgdomain.tools.qr import print_qr
 
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
-def reset_all(config_file: str):
+@click.option('--qr', is_flag=True, default=False, help="Generate a QR code of a config file")
+@click.option('--no-save', is_flag=True, default=False, help="Don't save the config to file")
+def reset_all(config_file: str, qr: bool, no_save: bool):
     """Resets configuration to accept all domains."""
 
     config = parse_config_file(config_file)
     config.set("Peer", "AllowedIPs", "0.0.0.0/0, ::/0")
 
-    with open(config_file, "w") as cfg_file:
-        config.write(cfg_file)
+    if not no_save:
+        with open(config_file, "w") as cfg_file:
+            config.write(cfg_file)
 
     click.echo(f"Resetting {config_file} to accept all domains.")
+
+    if qr:
+        print_qr(config)
