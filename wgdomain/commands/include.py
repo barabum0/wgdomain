8a63f4771477fc645791ede36762129943f3b43e
roman mac <barabum@duck.com>
2023-11-25 18:59:36 +0300

    :sparkles: feat: add QR code generation and config save options to 'include' command

--- wgdomain/commands/include.py
+++ wgdomain/commands/include.py
@@ -1,10 +1,35 @@
 import click
 
+from wgdomain.tools.config import parse_config_file
+from wgdomain.tools.domain import get_A_records
+from wgdomain.tools.qr import print_qr
+
 
 @click.command()
 @click.argument('config_file', type=click.Path(exists=True, dir_okay=False))
 @click.argument('domain')
-def include(config_file: str, domain: str):
+@click.option('--qr', is_flag=True, default=False, help="Generate a QR code of a config file")
+@click.option('--no-save', is_flag=True, default=False, help="Don't save the config to file")
+def include(config_file: str, domain: str, qr: bool, no_save: bool):
     """Includes a domain or IP for routing through Wireguard."""
-    # Code to include domain/IP
-    click.echo(f"Including {domain} in {config_file}.")
+
+    config = parse_config_file(config_file)
+
+    ips = get_A_records(domain)
+    ips = [f"{ip}/32" for ip in ips]
+
+    allowed_ips = config.get("Peer", "AllowedIPs").strip(",")
+    allowed_ips = [ip.strip() for ip in allowed_ips]
+    allowed_ips += ips
+    allowed_ips = list(set(allowed_ips))
+
+    config.set("Peer", "AllowedIPs", ", ".join(allowed_ips))
+
+    if not no_save:
+        with open(config_file, "w") as cfg_file:
+            config.write(cfg_file)
+
+    click.echo(f"Domain {domain} was included.")
+
+    if qr:
+        print_qr(config)
